a0c45d57cda8963784f911bf94e60e3559e81d4d
kyawoo-23 <kyawkyawjek@gmail.com>
2022-12-26 23:27:29 +0630

    build error

--- src/components/TeamPokeCard.jsx
+++ src/components/TeamPokeCard.jsx
@@ -3,21 +3,21 @@ import { useFetchPokemonDetailsQuery } from "../store"
 import TeamCardButton from "./TeamCardButton"
 
 const TeamPokeCard = ({ member, teamId }) => {
-  const { data, error, isFetching } = useFetchPokemonDetailsQuery(member)
+  const { data, isFetching } = useFetchPokemonDetailsQuery(member)
 
   return (
-    <div className="bg-cardBg hover:bg-cardBgHover border-2 border-primary rounded-xl flex flex-col gap-y-3 items-center cursor-pointer p-3">
+    <div className='bg-cardBg hover:bg-cardBgHover border-2 border-primary rounded-xl flex flex-col gap-y-3 items-center cursor-pointer p-3'>
       {isFetching ? (
         <p>Loading {member} img</p>
       ) : (
         <img
-          className="w-38 h-38 object-contain"
+          className='w-38 h-38 object-contain'
           src={data.sprites.other.home.front_default}
           alt={member}
         />
       )}
-      <p className="font-medium capitalize">{member}</p>
-      <div className="flex gap-2 items-center w-full">
+      <p className='font-medium capitalize'>{member}</p>
+      <div className='flex gap-2 items-center w-full'>
         <TeamCardButton isReplace={true} teamId={teamId} member={member} />
         <TeamCardButton isReplace={false} teamId={teamId} member={member} />
       </div>
