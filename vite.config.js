4db04b1f5dccf1ba4b5345fdaee6839cf5518770
kyawoo-23 <kyawkyawjek@gmail.com>
2022-12-28 17:11:57 +0630

    title and favicon added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import htmlPlugin from "vite-plugin-html-config"
+
+const htmlPluginOpt = {
+  title: "Pokemon Team",
+  favicon: "./pikachu.png",
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), htmlPlugin(htmlPluginOpt)],
 })
